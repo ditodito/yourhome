74e412fb9f20866c92fa5cf8a7705143eb354e57
dkhanishvili <=>
2017-09-09 19:47:37 +0400

    vendor comit

--- frontend/web/js/site.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function() {
-
-    $("#languages").on("change", function() {
-        window.location.href = $(this).find("option:selected").data("url");
-        //console.log($(this).find("option:selected").data("url"));
-    });
-
-});
