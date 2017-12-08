61163737d0e2793412ca501e9045dcaa165434b0
dkhanishvili <dito_1987@mail.ru>
2017-12-08 13:04:19 +0400

    service page dot leader style

--- frontend/views/site/services.php
+++ frontend/views/site/services.php
@@ -9,20 +9,36 @@ $this->title = 'YourHomeHotel :: '.\Yii::t('menu', 'Service');
 
 <h3 class="page-title"><?=\Yii::t('services', 'Hotel facilities')?></h3>
 
-<div class="service-wrapper">
-    <div class="service-title"><?=\Yii::t('services', 'Hotel airport transfer (one way)')?>:</div>
-    <div class="service-item">- <?=\Yii::t('services', 'by comfortable minivan')?> .... 80 GEL</div>
-    <div class="service-item">- <?=\Yii::t('services', 'by car')?> .... 40 GEL</div>
-</div>
-<div class="service-wrapper">
-    <div class="service-title"><?=\Yii::t('services', 'Extra towel')?>: ..... 3 GEL</div>
-</div>
-<div class="service-wrapper">
-    <div class="service-title"><?=\Yii::t('services', 'Extra sofa bed per night')?>: ..... 25 GEL</div>
-</div>
-<div class="service-wrapper">
-    <div class="service-title"><?=\Yii::t('services', 'Toiletries (slippers, shampoo, toothpaste, toothbrush, shower gel)')?>: ..... 3 GEL</div>
-</div>
+<ul class="price-list">
+    <li><strong><?=\Yii::t('services', 'Hotel airport transfer (one way)')?></strong></li>
+    <li class="dotted" style="padding-left: 25px;">
+        <span>- <?=\Yii::t('services', 'by comfortable minivan')?></span>
+        <span>80 GEL</span>
+    </li>
+    <li class="dotted" style="padding-left: 25px;">
+        <span>- <?=\Yii::t('services', 'by car')?></span>
+        <span>40 GEL</span>
+    </li>
+    <li class="dotted">
+        <span><strong><?=\Yii::t('services', 'Extra towel')?></strong></span>
+        <span>3 GEL</span>
+    </li>
+    <li class="dotted">
+        <span><strong><?=\Yii::t('services', 'Extra sofa bed per night')?></strong></span>
+        <span>25 GEL</span>
+    </li>
+    <li class="dotted">
+        <span>
+            <strong><?=\Yii::t('rooms', 'Toiletries')?></strong>
+            (<?=\Yii::t('rooms', 'Slippers')?>,
+            <?=\Yii::t('rooms', 'Shampoo')?>,
+            <?=\Yii::t('rooms', 'Toothpaste')?>,
+            <?=\Yii::t('rooms', 'Toothbrush')?>,
+            <?=\Yii::t('rooms', 'Shower gel')?>)
+        </span>
+        <span>3 GEL</span>
+    </li>
+</ul>
 
 <ul class="service-list">
     <li><?=\Yii::t('main', 'Free')?> WiFi</li>
@@ -30,6 +46,7 @@ $this->title = 'YourHomeHotel :: '.\Yii::t('menu', 'Service');
     <li><?=\Yii::t('services', 'Free linens')?></li>
     <li><?=\Yii::t('services', 'Shared kitchenette (Microwave, Stovetop, Refrigerator, Kettle)')?></li>
     <li><?=\Yii::t('services', 'Shared bathroom (Hairdryer)')?></li>
+    <li><?=\Yii::t('services', 'Free private parking')?> (<?=\Yii::t('contacts', 'upon request')?>)</li>
     <li><?=\Yii::t('services', 'Luggage storage')?></li>
     <li><?=\Yii::t('services', 'Daily housekeeping')?></li>
     <li><?=\Yii::t('services', 'Non-smoking rooms')?></li>
